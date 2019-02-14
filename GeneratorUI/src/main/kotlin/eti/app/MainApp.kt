2a6823352ec3437410e9c93e9b24e49629128dfa
L. Wagner <lion.wagner@freenet.de>
2019-02-14 10:57:58 +0100

    missing chang from last commit

--- GeneratorUI/src/main/kotlin/eti/app/MainApp.kt
+++ GeneratorUI/src/main/kotlin/eti/app/MainApp.kt
@@ -4,10 +4,11 @@ import eti.view.MainView
 import javafx.stage.Stage
 import tornadofx.*
 
-class MyApp : App(MainView::class, Styles::class) {
+class MainApp : App(MainView::class, Styles::class) {
     override fun start(stage: Stage) {
         stage.height = 500.0
         stage.width = 850.0
+        stage.resizableProperty().set(false)
         super.start(stage)
     }
 }
